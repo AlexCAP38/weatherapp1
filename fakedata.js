999fe60074ddfb4792286fb2bfed80a7b0cc3aa6
Alexey Gribov <UltraAngarsk@gmail.com>
2025-10-17 22:42:30 +0700

    task-done

---
+++ fakedata.js
@@ -0,0 +1,9 @@
+export const weatherToday = {
+    humidity: 75,
+    barometer: 761,
+    visibility: 28,
+    sunrise: '8:32',
+    sunset: '16:37',
+    direction: 'Северо-западный',
+    speed: 2
+}
--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-const searchBar = document.querySelector('#search-bar');
-searchBar && searchBar.addEventListener('input', callback);
+import {weatherToday} from './fakedata.js';
 
+const searchBar = document.querySelector('#search-bar');
+const humidity = document.querySelector('#humidity-value');
+const humidityBar = document.querySelector('#humidity-bar');
+const barometer = document.querySelector('#barometer');
+const barometerBar = document.querySelector('#barometer-bar');
+const barometerDescription = document.querySelector('#barometer-description');
+const visibility = document.querySelector('#visibility');
+const visibilityBar = document.querySelector('#visibility-bar');
+const visibilityDescription = document.querySelector('#visibility-description');
+const sunrise = document.querySelector('#sunrise');
+const sunriseDescription = document.querySelector('#sunrise-description');
+const sunset = document.querySelector('#sunset');
+const sunsetDescription = document.querySelector('#sunset-description');
+const speed = document.querySelector('#speed');
+const direction = document.querySelector('#direction');
+
+
+searchBar && searchBar.addEventListener('input', callback);
 function callback(event) {
     console.log(event.target.value);
 }
+
+humidity.textContent = `${weatherToday.humidity} %`;
+humidityBar.value = weatherToday.humidity;
+
+barometer.textContent = weatherToday.barometer;
+barometerBar.value = weatherToday.barometer;
+barometerDescription.textContent = 'нормально';
+
+visibility.textContent = weatherToday.visibility;
+visibilityBar.value = weatherToday.visibility;
+visibilityDescription.textContent = 'нормально';
+
+sunrise.textContent = weatherToday.sunrise;
+sunriseDescription.textContent = `Прошло: 02:47`;
+
+sunset.textContent = weatherToday.sunset;
+sunsetDescription.textContent = `Осталось: 05:08`;
+
+speed.textContent = `${weatherToday.speed} м/с`;
+direction.textContent = weatherToday.direction;
